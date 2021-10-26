c5e100ec6d0759468fd2989102babd1651959fa7
Jorge Armando Armando <80140764+jorge-armando-navarro-flores@users.noreply.github.com>
2021-10-26 19:04:00 +0000

    maximum_circular_sum_subarray

--- algorithms/arrays/maximum_circular_sum_subarray.py
+++ algorithms/arrays/maximum_circular_sum_subarray.py
@@ -1,18 +1,22 @@
 def maximum_circular_sum_subarray(arr):
   n = len(arr)
+  max_normal = maximum_subarray_sum(arr)
+  if max_normal < 0:
+    return max_normal
+  arr_sum = 0
+  for i in range(n):
+    arr_sum += arr[i]
+    arr[i] = -arr[i]
+  max_circular = arr_sum + maximum_subarray_sum(arr)
+  return max(max_normal, max_circular)
+
+def maximum_subarray_sum(arr):
+  n = len(arr)
   maxEnding = arr[0]
   maxSum = arr[0]
   for i in range(1,n):
     maxEnding = max(maxEnding + arr[i], arr[i])
     if maxEnding > maxSum:
       maxSum = maxEnding
-
-  maxCircularEnding = maxSum
-  maxCircularSum = maxSum
-  for i in range(n-1):
-    print(maxCircularEnding)
-    maxCircularEnding = max(maxCircularEnding + arr[i], arr[i])
-    if maxCircularEnding > maxCircularSum:
-      maxCircularSum = maxCircularEnding
-  return max(maxSum, maxCircularSum)
-  return maxCircularSum
+  return maxSum 
+  
